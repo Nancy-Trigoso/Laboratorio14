ccc8a492ce50a41f4d68614ebdd67826160df5f4
Yulber Espinoza <yulbertime@gmail.com>
2024-07-18 22:51:28 -0500

    Subir trabajo

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById("chart");
 
 /* TODO:
  * - Instantiate a new AppState
@@ -11,9 +11,65 @@ let canvasElem = document.getElementById('chart')
  *
  */
 
-
-
 function renderChart() {
+  const state = new AppState();
+
+  state.loadItems();
+
+  const productsName = [];
+  const productTimesClicked = [];
+  const productTimesShown = [];
+
+  for (let i = 0; i < state.allProducts.length; i++) {
+    productsName.push(state.allProducts[i].name);
+    productTimesClicked.push(state.allProducts[i].timesClicked);
+    productTimesShown.push(state.allProducts[i].timesShown);
+  }
+
+  let chartObj = {
+    type: "bar",
+    data: {
+      labels: productsName,
+      datasets: [
+        {
+          label: "#of votes",
+          data: productTimesClicked,
+          borderwidth: 1,
+          backgroundcolor: ["#ff006e"],
+        },
+        {
+          label: "#of views",
+          data: productTimesShown,
+          borderwidth: 1,
+          backgroundcolor: ["#ff006e"],
+        },
+      ],
+    },
+    options: {
+      indexAxis: "y",
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: { color: "black" },
+        },
+        x: {
+          ticks: { color: "black" },
+        },
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: "black",
+            padding: 30,
+            font: {
+              size: 16,
+            },
+          },
+        },
+      },
+    },
+  };
+  new Chart(canvasElem, chartObj);
 }
 
 renderChart();
